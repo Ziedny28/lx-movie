a6df706a6a6970bf2f64d14bc7ea3e190d93f48d
Ziednyea <mubarokziedny@gmail.com>
2022-12-29 10:50:59 +0700

    first commit

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from os.path import join, dirname
 from dotenv import load_dotenv
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
+from http import client
+import requests
+from bs4 import BeautifulSoup
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
